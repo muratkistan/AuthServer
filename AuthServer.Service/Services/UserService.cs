4bc37a32eb8b18d5d4f170c0f80cdd2ca4ce5b43
muratkistan <muratkistan@gmail.com>
2024-08-21 17:26:55 +0300

    user service tamamlandi.

---
+++ AuthServer.Service/Services/UserService.cs
@@ -0,0 +1,61 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<UserApp> _userManager;
+
+        public UserService(UserManager<UserApp> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto)
+        {
+            var user = new UserApp
+            {
+                UserName = createUserDto.UserName,
+                Email = createUserDto.Email
+            };
+            var result = await _userManager.CreateAsync(user, createUserDto.Password);
+
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(x => x.Description).ToList();
+                return Response<UserAppDto>.Fail(new ErrorDto(errors, true), 400);
+
+                //var error = string.Join(",", errors);
+                //return Response<UserAppDto>.Fail(error, 500, true);
+            }
+
+            return Response<UserAppDto>.Success(ObjectMapper.Mapper.Map<UserAppDto>(user), 200);
+        }
+
+        public Task<Response<NoContent>> CreateUserRoles(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Response<UserAppDto>> GetUserByNameAsync(string userName)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+
+            if (user is null)
+            {
+                return Response<UserAppDto>.Fail("Username not found", 404, true);
+            }
+            return Response<UserAppDto>.Success(ObjectMapper.Mapper.Map<UserAppDto>(user), 200);
+        }
+    }
+}
