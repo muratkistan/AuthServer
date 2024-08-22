fe4c2342fd39d1b49e2fcf1a116778b2287a804b
muratkistan <muratkistan@gmail.com>
2024-08-22 17:42:28 +0300

    product controller eklendi

---
+++ AuthServer.API/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : CustomBaseController
+    {
+        private readonly IGenericService<Product, ProductDto> _productService;
+
+        public ProductController(IGenericService<Product, ProductDto> productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return ActionResultInstance(await _productService.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return ActionResultInstance(await _productService.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.AddAsync(productDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.Update(productDto, productDto.Id));
+        }
+
+        [HttpDelete("/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return ActionResultInstance(await _productService.Remove(id));
+        }
+    }
+}
